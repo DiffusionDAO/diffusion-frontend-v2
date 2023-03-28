472baf05cf4d616b8d24358d67d0df08a2d1bf9a
DiffusionDAO <diffusiondao@protonmail.com>
2023-03-28 14:02:20 +0800

    update usePollBlockNumber

--- apps/web/src/utils/providers.ts
+++ apps/web/src/utils/providers.ts
@@ -1,6 +1,6 @@
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
 
-export const BSC_PROD_NODE = process.env.NEXT_PUBLIC_NODE_PRODUCTION || 'https://bsc-dataseed1.defibit.io'
+export const BSC_PROD_NODE = process.env.NEXT_PUBLIC_NODE_PRODUCTION || 'https://rpc.diffusiondao.org'
 
 export const bscRpcProvider = new StaticJsonRpcProvider(BSC_PROD_NODE)
 
